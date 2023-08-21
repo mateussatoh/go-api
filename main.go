51f061510d6e4a70f4eb8a6e11c4dfe78f9ce32d
mateussatoh <mateussatoh@gmail.com>
2023-08-21 19:51:53 -0300

    feat: add create method

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ type album struct {
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
 
 	router.Run("localhost:8080")
 }
@@ -28,4 +29,18 @@ var albums = []album{
 	{Id: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
 	{Id: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
 	{Id: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
